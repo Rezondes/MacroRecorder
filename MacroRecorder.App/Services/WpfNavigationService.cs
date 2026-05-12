da69ad35dbaf51eb1e39a0b26484e5f42b8db396
Rezondes <stevenrichter1902@gmail.com>
2026-05-12 19:06:26 +0200

    fix(app): construct editor and record VMs without ActivatorUtilities

--- MacroRecorder.App/Services/WpfNavigationService.cs
+++ MacroRecorder.App/Services/WpfNavigationService.cs
@@ -1,5 +1,7 @@
 using System.Windows;
 using MacroRecorder.App.ViewModels;
+using MacroRecorder.Application;
+using MacroRecorder.Application.Ports;
 using MacroRecorder.Domain;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,30 +11,47 @@ public sealed class WpfNavigationService(IServiceProvider serviceProvider) : INa
 {
     public void OpenEditor(MacroId id, Action? onMacroSaved = null)
     {
-        var editorViewModel = ActivatorUtilities.CreateInstance<MacroEditorViewModel>(
-            serviceProvider,
-            new object[] { id, true, null!, onMacroSaved! });
+        var editorViewModel = CreateEditorViewModel(id, loadFromDisk: true, inMemoryMacro: null, onMacroSaved);
         var editorWindow = new MacroEditorWindow(editorViewModel) { Owner = System.Windows.Application.Current?.MainWindow };
         editorWindow.Show();
     }
 
     public void OpenNewMacroEditor(Macro macro, Action? onMacroSaved = null)
     {
-        var editorViewModel = ActivatorUtilities.CreateInstance<MacroEditorViewModel>(
-            serviceProvider,
-            new object[] { macro.Id, false, macro, onMacroSaved! });
+        var editorViewModel = CreateEditorViewModel(macro.Id, loadFromDisk: false, macro, onMacroSaved);
         var editorWindow = new MacroEditorWindow(editorViewModel) { Owner = System.Windows.Application.Current?.MainWindow };
         editorWindow.Show();
     }
 
     public void OpenRecording(Action? onClosed = null, Action? onMacroSavedFromEditor = null)
     {
-        var recordViewModel = ActivatorUtilities.CreateInstance<RecordViewModel>(
-            serviceProvider,
-            new object[] { onMacroSavedFromEditor! });
+        var recordViewModel = new RecordViewModel(
+            serviceProvider.GetRequiredService<RecordingCoordinator>(),
+            serviceProvider.GetRequiredService<MacroWorkspaceService>(),
+            serviceProvider.GetRequiredService<IUserDialogService>(),
+            serviceProvider.GetRequiredService<INavigationService>(),
+            serviceProvider.GetRequiredService<IUiLocalizer>(),
+            onMacroSavedFromEditor);
         var recordWindow = new RecordWindow(recordViewModel) { Owner = System.Windows.Application.Current?.MainWindow };
         if (onClosed is not null)
             recordWindow.Closed += (_, _) => onClosed();
         recordWindow.Show();
     }
+
+    private MacroEditorViewModel CreateEditorViewModel(
+        MacroId macroId,
+        bool loadFromDisk,
+        Macro? inMemoryMacro,
+        Action? onMacroSaved) =>
+        new MacroEditorViewModel(
+            serviceProvider.GetRequiredService<MacroWorkspaceService>(),
+            serviceProvider.GetRequiredService<IUserDialogService>(),
+            serviceProvider.GetRequiredService<IEditorInsertDialogs>(),
+            serviceProvider.GetRequiredService<IPlaybackService>(),
+            serviceProvider.GetRequiredService<RecordingCoordinator>(),
+            serviceProvider.GetRequiredService<IUiLocalizer>(),
+            macroId,
+            loadFromDisk,
+            inMemoryMacro,
+            onMacroSaved);
 }
